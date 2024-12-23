84fa9b98df7fee0fc7b3627466e5a39aa4e2fa4d
buddy <Pro100biv@mail.ru>
2024-12-23 17:43:07 +0400

    final hash table version

--- algorithm/algorithm.h
+++ algorithm/algorithm.h
@@ -3,270 +3,299 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include<random>
+#include <random>
 
 using namespace std;
 
-
 namespace algorithm {
-	template <typename K, typename T>
-	class HashTable {
-	private:
-		template <typename K, typename T>
-		struct HashPair {
-			K _key;
-			T _value;
-			HashPair* _next;
-			bool _filled;
-			HashPair(K key, T value) : _key(key), _value(value), _filled(true), _next(nullptr) {}
-			HashPair() : _key(K()), _value(T()), _filled(false), _next(nullptr) {}
-		};
-		vector<HashPair<K, T>> _data;
-		int _count = 0;
-		size_t _size;
-		size_t hash_function(K key) {
-			size_t a = 2654435761;
-			size_t w = sizeof(int) * 8;
-			size_t l = log2(_data.size());
-			size_t result = ((key * a) % static_cast<size_t>(pow(2, w))) >> (w - l);
-			return result;
-		}
-		int random(int a, int b) {
-			random_device random_device;
-			mt19937 generator(random_device());
-			uniform_int_distribution<> distribution(a, b);
-			return distribution(generator);
-		}
-	public:
-		HashTable(int size);
-		~HashTable();
-		HashTable(const HashTable& other);
-		HashTable(int size, int min_key, int max_key, int min_value, int max_value);
-		void print();
-		void insert(K key, T value);
-		void insert_or_assign(K key, T value);
-		bool contains(T value);
-		T* search(K key);
-		bool erase(K key);
-		int count(K key);
-		HashTable<K, T>& operator=(const HashTable<K, T>& other);
-		int get_count();
-		int get_size();
-	};
+    template <typename K, typename T>
+    class HashTable {
+    private:
+        template <typename K, typename T>
+        struct HashPair {
+            K _key;
+            T _value;
+            HashPair* _next;
+            bool _filled;
+            HashPair(K key, T value) : _key(key), _value(value), _filled(true), _next(nullptr) {}
+            HashPair() : _key(K()), _value(T()), _filled(false), _next(nullptr) {}
+        };
 
-	template<typename K, typename T>
-	HashTable<K, T>::HashTable(int size) : _size(size)
-	{
-		_data.resize(_size);
-	}
+        vector<HashPair<K, T>> _data;
+        int _count = 0;
+        size_t _size;
+        bool rehashing_enabled = false;
 
-	template<typename K, typename T>
-	HashTable<K, T>::~HashTable() {
-		for (size_t i = 0; i < _size; ++i) {
-			HashPair<K, T>* current = _data[i]._next;
-			while (current) {
-				HashPair<K, T>* to_delete = current;
-				current = current->_next;
-				delete to_delete;
-			}
-			_data[i]._next = nullptr;
-		}
-	}
+        size_t hash_function(K key) {
+            size_t a = 2654435761;
+            size_t w = sizeof(int) * 8;
+            size_t l = log2(_data.size());
+            size_t result = ((key * a) % static_cast<size_t>(pow(2, w))) >> (w - l);
+            return result;
+        }
+
+        void rehash() {
+            size_t new_size = _size * 2;
+            vector<HashPair<K, T>> old_data = std::move(_data);
+            _data.resize(new_size);
+            _size = new_size;
+            _count = 0;
+
+            for (auto& pair : old_data) {
+                HashPair<K, T>* current = &pair;
+                while (current && current->_filled) {
+                    insert(current->_key, current->_value);
+                    current = current->_next;
+                }
+            }
+        }
+
+        int random(int a, int b) {
+            random_device random_device;
+            mt19937 generator(random_device());
+            uniform_int_distribution<> distribution(a, b);
+            return distribution(generator);
+        }
+
+    public:
+        HashTable(int size);
+        ~HashTable();
+        HashTable(const HashTable& other);
+        HashTable(int size, int min_key, int max_key, int min_value, int max_value);
+        void print();
+        void insert(K key, T value);
+        void insert_or_assign(K key, T value);
+        bool contains(T value);
+        T* search(K key);
+        bool erase(K key);
+        int count(K key);
+        HashTable<K, T>& operator=(const HashTable<K, T>& other);
+        int get_count();
+        int get_size();
+
+        void enable_rehashing();
+        void disable_rehashing();
+    };
+
+    template<typename K, typename T>
+    HashTable<K, T>::HashTable(int size) : _size(size) {
+        _data.resize(_size);
+    }
+
+    template<typename K, typename T>
+    HashTable<K, T>::~HashTable() {
+        for (size_t i = 0; i < _size; ++i) {
+            HashPair<K, T>* current = _data[i]._next;
+            while (current) {
+                HashPair<K, T>* to_delete = current;
+                current = current->_next;
+                delete to_delete;
+            }
+            _data[i]._next = nullptr;
+        }
+    }
+
+    template<typename K, typename T>
+    HashTable<K, T>::HashTable(const HashTable& other) : _size(other._size), _count(other._count) {
+        _data.resize(_size);
+        for (size_t i = 0; i < _size; ++i) {
+            HashPair<K, T>* current = other._data[i]._next;
+            while (current) {
+                insert(current->_key, current->_value);
+                current = current->_next;
+            }
+        }
+    }
+
+    template<typename K, typename T>
+    HashTable<K, T>::HashTable(int size, int min_key, int max_key, int min_value, int max_value) : _size(size) {
+        _data.resize(_size);
+        for (int i = 0; i < 25; ++i) {
+            K key = random(min_key, max_key);
+            T value = random(min_value, max_value);
+            insert(key, value);
+        }
+    }
+
+    template<typename K, typename T>
+    void HashTable<K, T>::enable_rehashing() {
+        rehashing_enabled = true;
+    }
+
+    template<typename K, typename T>
+    void HashTable<K, T>::disable_rehashing() {
+        rehashing_enabled = false;
+    }
+    template<typename K, typename T>
+    void HashTable<K, T>::print()
+    {
+        for (int i = 0; i < _data.size(); i++) {
+            cout << "Bucket: " << i << endl;
+            HashPair<K, T>* temp = &_data[i];
+            while (temp) {
+                cout << "{" << "Key: " << temp->_key << " Value: " << temp->_value << "}" << endl;
+                temp = temp->_next;
+            }
+            cout << endl;
+        }
+    }
+    template<typename K, typename T>
+    void HashTable<K, T>::insert(K key, T value) {
+        if (rehashing_enabled && _count >= _size * 0.75) {
+            rehash();
+        }
+
+        size_t index = hash_function(key);
+        HashPair<K, T>* temp = &_data[index];
+        if (temp->_filled) {
+            while (temp->_next) {
+                temp = temp->_next;
+            }
+            temp->_next = new HashPair<K, T>(key, value);
+            temp->_next->_filled = true;
+        }
+        else {
+            _data[index] = HashPair<K, T>(key, value);
+            _data[index]._filled = true;
+        }
+        _count++;
+    }
+
+    template<typename K, typename T>
+    void HashTable<K, T>::insert_or_assign(K key, T value) {
+        size_t index = hash_function(key);
+        HashPair<K, T>* temp = &_data[index];
+        while (temp) {
+            if (temp->_key == key) {
+                temp->_value = value;
+                return;
+            }
+            temp = temp->_next;
+        }
+        insert(key, value);
+    }
+
+    template<typename K, typename T>
+    bool HashTable<K, T>::contains(T value) {
+        for (auto& pair : _data) {
+            HashPair<K, T>* temp = &pair;
+            while (temp) {
+                if (temp->_value == value) {
+                    return true;
+                }
+                temp = temp->_next;
+            }
+        }
+        return false;
+    }
+
+    template<typename K, typename T>
+    T* HashTable<K, T>::search(K key) {
+        size_t index = hash_function(key);
+        if (!_data[index]._filled) {
+            return nullptr;
+        }
+        else {
+            HashPair<K, T>* temp = &_data[index];
+            while (temp) {
+                if (temp->_key == key) {
+                    return &temp->_value;
+                }
+                temp = temp->_next;
+            }
+            return nullptr;
+        }
+    }
+
+    template<typename K, typename T>
+    bool HashTable<K, T>::erase(K key) {
+        size_t index = hash_function(key);
+        HashPair<K, T>* current = &_data[index];
+        HashPair<K, T>* prev = nullptr;
+
+        while (current) {
+            if (current->_key == key && current->_filled) {
+                if (prev) {
+                    prev->_next = current->_next; 
+                    delete current;             
+                }
+                else {
+                    if (current->_next) {
+                        HashPair<K, T>* next = current->_next;
+                        _data[index] = *next;    
+                        delete next;            
+                    }
+                    else {
+                        _data[index]._filled = false; 
+                        _data[index]._next = nullptr;
+                    }
+                }
+                _count--; 
+                return true;
+            }
+            prev = current;
+            current = current->_next;
+        }
+        return false;
+    }
 
 
-	template<typename K, typename T>
-	HashTable<K, T>::HashTable(const HashTable& other) : _size(other._size), _count(other._count) {
-		_data.resize(_size);
-		for (size_t i = 0; i < _size; ++i) {
-			HashPair<K, T>* current = other._data[i]._next;
-			while (current) {
-				insert(current->_key, current->_value);
-				current = current->_next;
-			}
-		}
-	}
+    template<typename K, typename T>
+    int HashTable<K, T>::count(K key) {
+        size_t index = hash_function(key);
+        int count = 0;
+        HashPair<K, T>* current = &_data[index];
+        while (current) {
+            count++;
+            current = current->_next;
+        }
+        return count;
+    }
 
-	template<typename K, typename T>
-	HashTable<K, T>::HashTable(int size, int min_key, int max_key, int min_value, int max_value) : _size(size)
-	{
-		_data.resize(_size);
-		for (int i = 0; i < 25; ++i) {
-			K key = random(min_key, max_key);
-			T value = random(min_value, max_value);
-			insert(key, value);
-		}
-	}
-	template<typename K, typename T>
-	void HashTable<K, T>::print()
-	{
-		for (int i = 0; i < _data.size(); i++) {
-			cout << "Bucket: " << i << endl;
-			HashPair<K, T>* temp = &_data[i];
-			while (temp) {
-				cout << "{" << "Key: " << temp->_key << " Value: " << temp->_value << "}" << endl;
-				temp = temp->_next;
-			}
-			cout << endl;
-		}
-	}
+    template<typename K, typename T>
+    HashTable<K, T>& HashTable<K, T>::operator=(const HashTable<K, T>& other) {
+        if (this == &other) {
+            return *this;
+        }
 
-	template<typename K, typename T>
-	void HashTable<K, T>::insert(K key, T value)
-	{
-		size_t index = hash_function(key);
-		HashPair<K, T>* temp = &_data[index];
-		if (temp->_filled) {
-			while (temp->_next) {
-				temp = temp->_next;
-			}
-			temp->_next = new HashPair<K, T>(key, value);
-			temp->_next->_filled = true;
-		}
-		else {
-			_data[index] = HashPair<K, T>(key, value);
-			_data[index]._filled = true;
-		}
+        for (size_t i = 0; i < _size; ++i) {
+            HashPair<K, T>* current = _data[i]._next;
+            while (current) {
+                HashPair<K, T>* to_delete = current;
+                current = current->_next;
+                delete to_delete;
+            }
+            _data[i]._next = nullptr;
+        }
 
-	}
+        _size = other._size;
+        _count = other._count;
+        _data.resize(_size);
 
-	template<typename K, typename T>
-	void HashTable<K, T>::insert_or_assign(K key, T value) {
-		size_t index = hash_function(key);
-		HashPair<K, T>* temp = &_data[index];
-		while (temp) {
-			if (temp->_key == key) {
-				temp->_value = value;
-				return;
-			}
-			temp = temp->_next;
-		}
-		insert(key, value);
-	}
+        for (size_t i = 0; i < _size; ++i) {
+            HashPair<K, T>* current = other._data[i]._next;
+            while (current) {
+                insert(current->_key, current->_value);
+                current = current->_next;
+            }
+        }
 
+        return *this;
+    }
 
-	template<typename K, typename T>
-	bool HashTable<K, T>::contains(T value) {
-		for (auto& pair : _data) {
-			HashPair<K, T>* temp = &pair;
-			while (temp) {
-				if (temp->_value == value) {
-					return true;
-				}
-				temp = temp->_next;
-			}
-		}
-		return false;
-	}
+    template<typename K, typename T>
+    int HashTable<K, T>::get_count() {
+        int count_ = 0;
+        for (const auto& pair : _data) {
+            if (pair._filled) {
+                count_ += count(pair._key) - 1;
+            }
+        }
+        return count_;
+    }
 
-	template<typename K, typename T>
-	T* HashTable<K, T>::search(K key)
-	{
-		size_t index = hash_function(key);
-		if (!_data[index]._filled) {
-			return nullptr;
-		}
-		else {
-			HashPair<K, T>* temp = &_data[index];
-			while (temp) {
-				if (temp->_key == key) {
-					return &temp->_value;
-				}
-				temp = temp->_next;
-			}
-			return nullptr;
-		}
-	}
-
-	template<typename K, typename T>
-	bool HashTable<K, T>::erase(K key) {
-		size_t index = hash_function(key);
-		HashPair<K, T>* current = &_data[index];
-		HashPair<K, T>* prev = nullptr;
-
-		while (current) {
-			if (current->_key == key) {
-				if (prev) {
-					prev->_next = current->_next;
-					delete current;  
-				}
-				else {
-					if (current->_next) {
-						HashPair<K, T>* next = current->_next;
-						_data[index] = *next;
-						delete next;
-					}
-					else {
-						_data[index] = HashPair<K, T>();
-					}
-					delete current;
-
-				}
-				return true;
-			}
-			prev = current;
-			current = current->_next;
-		}
-		return false;  
-	}
-
-
-	template<typename K, typename T>
-	int HashTable<K, T>::count(K key) {
-		size_t index = hash_function(key);
-		int count = 0;
-		HashPair<K, T>* current = &_data[index];
-		while (current) {
-			count++;
-			current = current->_next;
-		}
-		return count;
-	}
-
-	template<typename K, typename T>
-	HashTable<K, T>& HashTable<K, T>::operator=(const HashTable<K, T>& other) {
-		if (this == &other) {
-			return *this;
-		}
-
-		for (size_t i = 0; i < _size; ++i) {
-			HashPair<K, T>* current = _data[i]._next;
-			while (current) {
-				HashPair<K, T>* to_delete = current;
-				current = current->_next;
-				delete to_delete;
-			}
-			_data[i]._next = nullptr;
-		}
-
-		_size = other._size;
-		_count = other._count;
-		_data.resize(_size);
-
-		for (size_t i = 0; i < _size; ++i) {
-			HashPair<K, T>* current = other._data[i]._next;
-			while (current) {
-				insert(current->_key, current->_value);
-				current = current->_next;
-			}
-		}
-
-		return *this;
-	}
-
-	template<typename K, typename T>
-	int HashTable<K, T>::get_count() {
-		int count_ = 0;
-		for (const auto& pair : _data) {
-			if (pair._filled) {
-				count_ += count(pair._key) - 1;
-			}
-		}
-		return count_;
-	}
-
-	template<typename K, typename T>
-	int HashTable<K, T>::get_size()
-	{
-		return _data.size();
-	}
+    template<typename K, typename T>
+    int HashTable<K, T>::get_size() {
+        return _data.size();
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,88 @@ int task(int experiments, int size) {
 	return sum;
 }
 
+
 int main() {
+    // Создаем хэш-таблицу фиксированного размера
+    HashTable<int, int> table(10);
 
+    // Включаем рехэширование
+    table.enable_rehashing();
+    cout << "Rehashing enabled" << endl;
 
-	for (int i = 25; i <= 475; i += 50) {
-		cout << "Size: " << i << endl;
-		cout << "Number of col: " << task(100, i) / 100.0 << endl;;
-		cout << endl;
-	}
-	
+    // Тестирование вставки
+    cout << "Inserting values..." << endl;
+    for (int i = 1; i <= 15; ++i) {
+        table.insert(i, i * 10);
+        cout << "Inserted: Key = " << i << ", Value = " << i * 10 << endl;
+    }
+
+    // Печать таблицы
+    cout << "\nHash Table after insertion:" << endl;
+    table.print();
+
+    // Тестирование поиска
+    cout << "\nSearching values..." << endl;
+    for (int i = 1; i <= 15; ++i) {
+        int* value = table.search(i);
+        if (value) {
+            cout << "Found: Key = " << i << ", Value = " << *value << endl;
+        }
+        else {
+            cout << "Key " << i << " not found" << endl;
+        }
+    }
+
+    // Тестирование удаления
+    cout << "\nDeleting keys 3, 7, 10..." << endl;
+    table.erase(3);
+    table.erase(7);
+    table.erase(10);
+
+    // Печать таблицы после удаления
+    cout << "\nHash Table after deletion:" << endl;
+    table.print();
+
+    // Проверка contains
+    cout << "\nChecking if values exist:" << endl;
+    for (int i = 1; i <= 15; ++i) {
+        if (table.contains(i * 10)) {
+            cout << "Value " << i * 10 << " exists in the table" << endl;
+        }
+        else {
+            cout << "Value " << i * 10 << " does not exist in the table" << endl;
+        }
+    }
+
+    // Тестирование insert_or_assign
+    cout << "\nTesting insert_or_assign..." << endl;
+    table.insert_or_assign(5, 999);
+    table.insert_or_assign(13, 888);
+    table.insert_or_assign(20, 777); // Новый ключ
+
+    // Печать таблицы после insert_or_assign
+    cout << "\nHash Table after insert_or_assign:" << endl;
+    table.print();
+
+    // Получение размера таблицы
+    cout << "\nHash Table size: " << table.get_size() << endl;
+
+    // Получение количества элементов
+    cout << "Hash Table element count: " << table.get_count() << endl;
+
+    // Отключение рехэширования
+    table.disable_rehashing();
+    cout << "\nRehashing disabled" << endl;
+
+    // Добавление новых значений без рехэширования
+    cout << "Inserting more values without rehashing..." << endl;
+    for (int i = 16; i <= 20; ++i) {
+        table.insert(i, i * 10);
+    }
+
+    // Печать таблицы
+    cout << "\nHash Table after inserting more values:" << endl;
+    table.print();
+
+    return 0;
 }
